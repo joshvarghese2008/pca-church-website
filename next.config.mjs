fbb1175c4005b32d842cb1d6836faeb2c77168b5
Josh Varghese <2008mlpy@gmail.com>
2024-12-21 11:41:25 +1100

    fixed css styling and added suffixes which browser could read

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-    webpack(webpackConfig) {
-        return {
-            ...webpackConfig,
-            optimization: {
-                minimize: false,
-            }
-        }
-    }
+    // webpack(webpackConfig) {
+    //     return {
+    //         ...webpackConfig,
+    //         optimization: {
+    //             minimize: false,
+    //         }
+    //     }
+    // }
 };
 
 export default nextConfig;
