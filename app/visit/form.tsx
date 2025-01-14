254ff023aa8d1b799f81f33ec32ea72610ae71fd
Josh Varghese <2008mlpy@gmail.com>
2025-01-14 15:28:15 +1100

    add a calendar

--- app/visit/form.tsx
+++ app/visit/form.tsx
@@ -15,6 +15,15 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Calendar } from "@/components/ui/calendar";
+import { CalendarIcon } from "lucide-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
 
 const formSchema = z.object({
   email: z.string().email({
@@ -22,6 +31,9 @@ const formSchema = z.object({
   }),
   firstName: z.string().min(2, {
     message: "Please enter a valid first name",
+  }),
+  visitDate: z.date({
+    required_error: "A Visit date is required",
   }),
 });
 
@@ -35,7 +47,6 @@ export default function VisitForm() {
   });
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
-
     try {
       const response = await fetch("../api/emails", {
         method: "POST",
@@ -96,6 +107,47 @@ export default function VisitForm() {
                 <FormControl>
                   <Input placeholder="First Name" {...field} />
                 </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="visitDate"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Date of birth</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-[240px] pl-3 text-left font-normal",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={{
+                        dayOfWeek: [1, 2, 3, 4, 5, 6],
+                      }}
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
                 <FormMessage />
               </FormItem>
             )}
