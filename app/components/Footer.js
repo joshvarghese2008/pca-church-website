9f3539e2c9e4e25aa03b4ed763039ac92b22a9c4
joshvarghese2008 <89509991+joshvarghese2008@users.noreply.github.com>
2025-01-26 23:45:42 +1100

    Adjust footer colouring

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,39 +1,58 @@
-'use client'
+"use client";
 
-import Link from "next/link"
-import styles from "./components.module.css"
-import { FacebookRounded } from "@mui/icons-material"
-import { YouTube } from "@mui/icons-material"
-import { TransitionLink } from "./utils/TransitionLink"
+import Link from "next/link";
+import styles from "./components.module.css";
+import { FacebookRounded } from "@mui/icons-material";
+import { YouTube } from "@mui/icons-material";
+import { TransitionLink } from "./utils/TransitionLink";
 
 export default function Footer() {
-    return (
-        <div className={styles.footer}>
-            <div className={styles.footercentre}>
-                <div className={styles.footercontent}>
-                    <div className={styles.footerlinks}>
-                        <div className={styles.footerwebsitelinkscontainer}>
-                            <TransitionLink href={'/'} className={styles.footerwebsitelink}>Home</TransitionLink>
-                            {/* <Link href={'/'} className={styles.footerwebsitelink}>Our Services</Link> */}
-                            <TransitionLink href={'/believe'} className={styles.footerwebsitelink}>What We Believe</TransitionLink>
-                            <TransitionLink href={'/give'} className={styles.footerwebsitelink}>Give</TransitionLink>
-                            <TransitionLink href={'/visit'} className={styles.footerwebsitelink}>Plan A Visit</TransitionLink>
-                        </div>
-                        <div className={styles.footerexternallinkscontainer}>
-                            {/* <Link href={'/'}>
+  return (
+    <div className={styles.footer}>
+      <div className={styles.footercentre}>
+        <div className={styles.footercontent}>
+          <div className={styles.footerlinks}>
+            <div className={styles.footerwebsitelinkscontainer}>
+              <TransitionLink href={"/"} className={styles.footerwebsitelink}>
+                Home
+              </TransitionLink>
+              {/* <Link href={'/'} className={styles.footerwebsitelink}>Our Services</Link> */}
+              <TransitionLink
+                href={"/believe"}
+                className={styles.footerwebsitelink}
+              >
+                What We Believe
+              </TransitionLink>
+              <TransitionLink
+                href={"/give"}
+                className={styles.footerwebsitelink}
+              >
+                Give
+              </TransitionLink>
+              <TransitionLink
+                href={"/visit"}
+                className={styles.footerwebsitelink}
+              >
+                Plan A Visit
+              </TransitionLink>
+            </div>
+            <div className={styles.footerexternallinkscontainer}>
+              {/* <Link href={'/'}>
                                 <FacebookRounded />
                             </Link>
                             <Link href={'/'}>
                                 <YouTube />
                             </Link> */}
-                        </div>
-                    </div>
-                    <hr className={styles.footerdivider} />
-                    <div className={styles.footercopyright}>
-                        <p>&copy; {new Date().getFullYear()} PCA Church. All rights reserved</p>
-                    </div>
-                </div>
             </div>
+          </div>
+          <hr className={styles.footerdivider} />
+          <div className={styles.footercopyright}>
+            <p style={{ color: "black" }}>
+              &copy; {new Date().getFullYear()} PCA Church. All rights reserved
+            </p>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
