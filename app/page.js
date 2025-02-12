c6c20e0d2a157eef88800752c92623657acc85b4
joshvarghese2008 <89509991+joshvarghese2008@users.noreply.github.com>
2025-02-13 10:11:37 +1100

    remove website title for home page

--- app/page.js
+++ app/page.js
@@ -2,10 +2,6 @@ import Image from "next/image";
 import styles from "./page.module.css";
 // import { supabase } from "./components/utils/supabase";
 
-export const metadata = {
-  title: 'Home',
-}
-
 export default async function Home() {
   // const { data: notes } = await supabase.from("sermons").select();
 
