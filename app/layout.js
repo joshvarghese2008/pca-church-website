ec25798c8e05e291c532274f264c7563a5351197
joshvarghese2008 <89509991+joshvarghese2008@users.noreply.github.com>
2025-02-12 11:19:19 +1100

    improve layout page for better SEO

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,17 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import Footer from "./components/Footer";
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 import { Toaster } from "@/components/ui/toaster";
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,22 +24,44 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: {
-    default: "Malayalam - Pentecostal Christian Assembly, Ingleburn",
-  },
-  description: "Malaylam Pentecostal Church in Ingleburn, NSW",
-
+  title: "Malayalam - PCA Church",
+  description:
+    "Pentecostal Christian Assembly is a Malayalam church located in Ingleburn, New South Wales",
+  keywords: [
+    "Malayalam Church",
+    "Malayalam",
+    "Pentecostal Church",
+    "Pentecostal",
+    "Pentecost",
+    "Holy Spirit",
+    "Malayalam Worship",
+    "Spirit-filled Church",
+    "Christian",
+    "Christian Church",
+    "Christian Worship"
+  ],
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+    icons: {
+      icon: 'favicon.ico'
+    }
+  }
 };
 
 const monotypeCorsiva = localFont({
-  src: '../public/customfonts/monotype-corsiva.ttf'
-})
+  src: "../public/customfonts/monotype-corsiva.ttf",
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={monotypeCorsiva.className}>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Navbar/>
+        <Navbar />
         {children}
         <Toaster />
         <Analytics />
